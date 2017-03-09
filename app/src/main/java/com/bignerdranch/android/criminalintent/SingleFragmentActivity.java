d12bb9654069376834b5c498e499a6b196ae895a
Avinash.Ganga <avi3110@gmail.com>
2017-03-09 23:44:49 +0200

    Chapter 9

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,38 +1,30 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by Avinash.Ganga on 2017/03/09.
+ */
 
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
 
     @Override
-    protected Fragment createFragment() {
-        return new CrimeFragment();
-    }
-
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-
         FragmentManager fm = getSupportFragmentManager();
-
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
-*/
-
-
-
-
+    }
 
 
 }

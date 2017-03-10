ac0f91244bcd3d86604952498f83733f5b2ffc97
Avinash.Ganga <avi3110@gmail.com>
2017-03-10 12:58:25 +0200

    Chapter 9

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -17,8 +17,10 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
         if (fragment == null) {
             fragment = createFragment();
             fm.beginTransaction()
